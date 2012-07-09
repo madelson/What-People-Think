17e58b247738f04192d57d1beaa4e46711e10b72
Michael Adelson <mike.adelson314@gmail.com>
2012-07-09 07:33:53 -0400

    Made the question page handler url-key-based rather than query string based

--- questionpage.py
+++ questionpage.py
@@ -18,35 +18,40 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 # asks the user a question
 class QuestionPage(webapp.RequestHandler):
-	def get(self, key):		
+	# ask the user a question
+	def get(self, question_id):		
 		id = handlers.get_user_id(self.request);
 		answerer = entities.Answerer.get_or_insert(id);	
 		common_values = handlers.get_template_values_common(answerer);
-
-		# determine the question to ask
 		
-		# if the user has answered all questions, redirect
-		if len(answerer.questions_answered) == len(question.all()):
-			self.redirect();
-			return;
-				
-		# if there is a question in the request AND it's valid, use that unless
-		# it's already been answered by the user
-		requested_question = self.request.get('question');
-		if requested_question in answerer.questions_answered:
-			self.redirect();
-			return;
-		if requested_question in question.all_by_id():
-			question_to_ask = requested_question;
-		else: # otherwise, choose a random question
+		# if no question_id was passed, pick a random question and redirect to that
+		if question_id is None or question_id == '':
 			already_answered = True;
 			while already_answered:
 				question_to_ask = question.all()[random.randint(0, len(question.all()) - 1)].id;
 				already_answered = question_to_ask in answerer.questions_answered;					
+			self.redirect('/ask/%s' % question_to_ask);
+			return;
+	
+		# error out on an invalid id since that's an invalid URL
+		if question_id not in question.all_by_id():
+			return handlers.bad_request_error('Invalid question identifier "%s"' % question_id);
+	
+		# determine the question to ask
+		
+		# if the user has answered all questions, redirect
+		if len(answerer.questions_answered) == len(question.all()):
+			self.redirect(); # TODO implement
+			return;
+					
+		# if they already answered the question, redirect to the result page
+		if question_id in answerer.questions_answered:
+			self.redirect('/result/%s' % question_id);
+			return;
 			
 		template_values = {
 			'template': handlers.template_path('question'),
-			'question': question.all_by_id()[question_to_ask],
+			'question': question.all_by_id()[question_id],
 			'dump': {
 				'id': handlers.get_user_id(self.request),
 				'key2': answerer.key().name()
@@ -56,29 +61,35 @@ class QuestionPage(webapp.RequestHandler):
 		path = handlers.template_path('index');
 		self.response.out.write(template.render(path, dict(common_values.items() + template_values.items())));
 	
-	def post(self):
+	def post(self, question_id):	
+		# check the question
+		if question_id not in question.all_by_id():
+			return handlers.bad_request_error('Invalid question "%s"' % question_id);
+
 		id = handlers.get_user_id(self.request);
 		answerer = entities.Answerer.get_by_key_name(id);
 		
-		# if no answerer could be found, redirect
+		# check the answerer
 		if answerer is None:
-			self.redirect();
+			return handlers.bad_request_error('Could not identify answerer "%s"' % id);
+		# if they've already answered, just redirect
+		if question_id in answerer.questions_answered:
+			self.redirect('/result/%s' % question_id);
 			return;
+		# check the answer
+		answer = self.request.get('answer');		
+		if int(answer) not in range(0, len(question.all_by_id()[question_id].answers)):
+			return handlers.bad_request_error('Invalid answer "%s" for question "%s"' % (answer, question_id)); 
 		
 		# otherwise, record the answer and redirect to the answer page
-		question_id = self.request.get('question');
-		answer = self.request.get('answer');		
-		if (question_id in question.all_by_id() 
-			and question_id not in answerer.questions_answered
-			and 0 <= int(answer) < len(question.all_by_id()[question_id].answers)):
-			# update the answerer
-			answerer.questions_answered.append(question_id);
-			answerer.answers.append(answer);
-			answerer.put();
-			
-			# update the answer counter
-			counter = entities.ShardedCounter(question_id + ":" + answer);
-			counter.increment();
+		# update the answerer
+		answerer.questions_answered.append(question_id);
+		answerer.answers.append(answer);
+		answerer.put();
+		
+		# update the answer counter
+		counter = entities.ShardedCounter(question_id + ':' + answer);
+		counter.increment();
 			
 		# redirect to the answer page for that question
-		self.redirect("/ask?question_id=%s&answer=%s" % (question_id, answer));
+		self.redirect("/ask/%s" % question_id);
